eec2687812e0673331669cc180bdae1ce58e4d2d
GlaucoTheDuck <maisgaludo@gmail.com>
2025-03-30 20:59:55 -0300

    second commit

--- App.js
+++ App.js
@@ -1,20 +1,82 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Image, Text } from 'react-native';
+import { useState } from 'react';
+import MetasList from './components/MetasList';
+import MetaInput from './components/MetaInput';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
+
+  const [metas, setMetas] = useState([]);
+
+  function adicionarMetaHandler(inputMeta){
+    const novaMeta = {id: Math.random().toString(), texto: inputMeta};
+    setMetas([...metas, novaMeta]);
+  }
+
+  function deletarMetaHandler(id){
+    const novasMetas = metas.filter(meta => meta.id !== id);
+    setMetas(novasMetas);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.topo}>
+      <View style={styles.imageContainer}>
+          <Image
+            source={require('./assets/favicon.png')}
+            style={styles.image}
+            resizeMode="contain"
+          />
+  </View>
+  <View >
+      <Text style={styles.headerText}>Minhas Metas</Text>
+      </View>
+  </View>
+
+      <View style={styles.mainContainer}>
+
+          <MetaInput onAddMeta={adicionarMetaHandler} />
+
+        <View style={styles.metaContainer}>
+            <MetasList array={metas} onDeleteItem={deletarMetaHandler} />
+        </View>
+      </View>
+    </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  mainContainer: {
+    padding: 30,
+    flex: 1,
+    flexDirection: 'column',
+  },
+
+  metaContainer: {
+    flex: 15,
+  },
+  safeArea: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  imageContainer: {
+    alignItems: 'left',
+    marginTop: 10,
+    paddingLeft: 30,
+  },
+  image: {
+    width: 50,
+    height: 50
+  },
+  topo: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center'
+  },
+  headerText: {
+    fontSize: 20,
+    marginLeft: 10
   },
 });
